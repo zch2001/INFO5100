ff8aee813178bc028b1811bdabe666e11f4f4ccb
zhouc <zhouchuhan010810@163.com>
2023-09-26 20:10:57 -0400

    subarray-homework

---
+++ Lab2/src/Subarray.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+
+
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+
+
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += array[j];
+                if (sum < 0) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
